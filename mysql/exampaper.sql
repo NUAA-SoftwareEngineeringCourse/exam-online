8ffa26a8c62a5fb52d851a9791ea008cd6fd7617
sinkinben <sinkinben@outlook.com>
2019-12-06 23:02:45 +0800

    fix exampaper.sql

--- mysql/exampaper.sql
+++ mysql/exampaper.sql
@@ -3,7 +3,7 @@ paper_id int(5) not null auto_increment,
 paper_title varchar(32) not null ,
 paper_desc varchar(1024) not null , -- paper desc is limited in 512 Chinese character
 paper_time int(3) not null ,
-paper_date date not null ,
+paper_date datetime not null ,
 paper_open int(1) not null ,
 paper_path varchar(256) not null ,
 paper_userid varchar(16) not null ,
